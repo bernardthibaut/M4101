d489edd009c7c16e86014540a211108ee85115a4
Hugo ALDER <hugoalder0@gmail.com>
2016-01-26 11:56:51 +0100

    La socket serveur

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc, char **argv)
+#include "socket.h"
+
+int main(void)
 {
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-if (argc > 1 && strcmp (argv [1], " - advice ") == 0) {
-	printf ("Don’t Panic !\n");
-	return 42;
-}
-	printf ("Need an advice ?\n");
+	creer_serveur(8080);
 	return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int creer_serveur(int port)
+{
+
+	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_serveur == -1)
+	{
+		perror("socket_serveur");
+		return -1;
+	}
+
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons (port);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+	{
+		perror ("bind socket_serveur");
+		return -1;
+	}
+
+	if(listen(socket_serveur, 10) == -1)
+	{
+		perror("listen socket_serveur");
+		return -1;
+	}
+
+	if(close(socket_serveur) == -1)
+	{
+		perror("close socket_serveur");
+		return -1;
+	}
+
+	return 0;
+}
